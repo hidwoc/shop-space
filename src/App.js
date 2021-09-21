507c60d468d75827edd48eee2b08080aa596715a
hidwoc <heidischoi@gmail.com>
2021-09-21 14:19:40 -0400

    install axios, successful GET from NASA api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect } from 'react';
+import { apiURL } from './services/apiConfig';
 
 function App() {
+  useEffect(() => {
+    const fetchData = async() => {
+      const res = await axios.get(apiURL)
+      console.log(res)
+    }
+    fetchData()
+  })
   return (
     <div className="App">
       APP
---
+++ src/services/apiConfig.js
@@ -0,0 +1,3 @@
+import { apiKey } from "./apiKey"
+
+export const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
