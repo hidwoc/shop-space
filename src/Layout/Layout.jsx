c7f1d1758d1dd469bd45d6695f9a06647c560b1d
hidwoc <heidischoi@gmail.com>
2021-09-21 16:38:03 -0400

    import Google fonts config app fonts set app titlle and favicon

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Layout.css"
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="layout">
       <header>Spacestagram</header>
       <div className="layout-children">{children}</div>
       <footer>
-        Images brought to you NASA's APOD API 🌙 Created by Heidi Choi
+        Images brought to you NASA's APOD API 🌙  Created by Heidi Choi
       </footer>
     </div>
   );
