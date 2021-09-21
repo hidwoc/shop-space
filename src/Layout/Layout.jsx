b3aef0f217fcc44a7897b8dcd346d45554b88543
hidwoc <heidischoi@gmail.com>
2021-09-21 16:42:58 -0400

    set colors

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import "./Layout.css"
+import "./Layout.css";
 
 const Layout = ({ children }) => {
   return (
     <div className="layout">
-      <header>Spacestagram</header>
+      <header>
+        <h1>Spacestagram</h1>
+      </header>
       <div className="layout-children">{children}</div>
       <footer>
-        Images brought to you NASA's APOD API 🌙  Created by Heidi Choi
+        Images brought to you NASA's APOD API 🌙 Created by Heidi Choi
       </footer>
     </div>
   );
